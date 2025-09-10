f6aeafce49185a8613afc25feda0ccda54e03aa0
Bruna Gomes <bruna.ckgomes@gmail.com>
2025-09-09 22:58:39 -0300

    Mudei a lista de nomes de usuários para tornarem-se objetos

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,12 +1,31 @@
-const usuarios = ['matheus', 'marcos', 'lucas', 'joão'];
+const usuarios = [
+    {
+        nome: 'Matheus',
+        email: 'matheus@gmail.com'
+    },
+    {
+        nome: 'marcos',
+        email: 'marcos@gmail.com'
+    },
+    {
+        nome: 'lucas',
+        email: 'lucas@gmail.com'
+    },
+    {
+        nome: 'joão',
+        email: 'joao@gmail.com'
+    }
+];
 
 function retornarUsuarios() {
     return usuarios;
 }
 
-function adicionarNovoUsuario(nome){
-    usuarios.push(nome);
+function adicionarNovoUsuario(usuario){ // esse usuario que inseri na funcao n precisa declarar
+    usuarios.push(usuario);
 }
+
+
 
 
 module.exports = {
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -5,13 +5,24 @@ const { expect } = require('chai');
 describe('Testar as funções de Gestão de usuários', function(){
     it('Validar que posso adicionar um novo nome de usuário na lista', function(){
         //1. Adicionar um novo nome na lista de usuários
-        adicionarNovoUsuario('Maria');
+        adicionarNovoUsuario(
+            {
+                nome: 'Maria',
+                email: 'maria@gmail.com'
+            },
+        );
 
         //2. Retornar a lista de usuários na caixa lista de usuários
         const listaDeUsuarios = retornarUsuarios();
 
         //3. Comparar se o novo nome está no fim da lista de usuários
-        expect(listaDeUsuarios.at(-1)).to.equal('Maria');
+       // expect(listaDeUsuarios.at(-1)).to.equal('Maria'); -> Ultimo nome
+       // expect(listaDeUsuarios.at(-1).nome).to.equal('Maria'); //-> ultimo nome que entrou no objeto
+
+        expect(listaDeUsuarios.at(-1)).to.eql({ //to.deep.equal | nesse exemplo vai comparar todo o objeto e não apenas um item.
+            nome: 'Maria',
+            email: 'maria@gmail.com'
+        })
 
     });
 
